30a9d67a70f4a256498adcc07409cf5ae130cebb
Dmytro Galin <mobiled2@gmail.com>
2017-01-14 14:38:53 +0100

    PreferencesFragment moved to PreferencesActivity

--- app/src/main/java/com/mobiled2/earthquake/PreferencesActivity.java
+++ app/src/main/java/com/mobiled2/earthquake/PreferencesActivity.java
@@ -4,11 +4,21 @@ import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 public class PreferencesActivity extends PreferenceActivity {
   public static final String PREF_AUTO_UPDATE = "PREF_AUTO_UPDATE";
   public static final String PREF_MIN_MAG = "PREF_MIN_MAG";
   public static final String PREF_UPDATE_FREQ = "PREF_UPDATE_FREQ";
+
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+  public static class PreferencesFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      addPreferencesFromResource(R.xml.preferences);
+    }
+  }
 
   @Override
   public void onCreate(Bundle savedInstanceState) {

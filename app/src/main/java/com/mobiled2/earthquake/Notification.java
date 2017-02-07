b188152054858343c30b58b1b4f7d6eeb8f9391a
Dmytro Galin <mobiled2@gmail.com>
2017-02-08 00:24:33 +0100

    Notifications moved to separate class

---
+++ app/src/main/java/com/mobiled2/earthquake/Notification.java
@@ -0,0 +1,87 @@
+package com.mobiled2.earthquake;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.support.v7.app.NotificationCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class Notification {
+  private static final int NOTIFICATION_ID = 1;
+
+  private static final double LIGHT_MAGNITUDE = 3;
+  private static final double MEDIUM_MAGNITUDE = 5;
+  private static final double HEAVY_MAGNITUDE = 7;
+
+  private static final int LIGHT_MAGNITUDE_COLOR = Color.GREEN;
+  private static final int MEDIUM_MAGNITUDE_COLOR = Color.YELLOW;
+  private static final int HEAVY_MAGNITUDE_COLOR = Color.RED;
+
+  private NotificationCompat.Builder notificationBuilder;
+  private Context context;
+
+  public Notification(Context context) {
+    notificationBuilder = new NotificationCompat.Builder(context);
+
+    notificationBuilder
+      .setAutoCancel(true)
+      .setDefaults(NotificationCompat.DEFAULT_ALL)
+      .setSmallIcon(R.drawable.ic_notification)
+      .setTicker("Earthquake detected");
+
+    this.context = context;
+  }
+
+  void send(QuakeData quakeData) {
+    Intent intent = new Intent(context, MainActivity.class);
+    long time = quakeData.getDate().getTime();
+    String date = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault()).format(time);
+    String details = quakeData.getDetails();
+    double magnitude = quakeData.getMagnitude();
+
+    intent.putExtra(ContentProvider.KEY_DATE, date);
+    intent.putExtra(ContentProvider.KEY_DETAILS, details);
+    intent.putExtra(ContentProvider.KEY_MAGNITUDE, String.valueOf(magnitude));
+    intent.putExtra(ContentProvider.KEY_LATITUDE, String.valueOf(quakeData.getLatitude()));
+    intent.putExtra(ContentProvider.KEY_LONGITUDE, String.valueOf(quakeData.getLongitude()));
+    intent.putExtra(ContentProvider.KEY_DEPTH, String.valueOf(quakeData.getDepth()));
+
+    PendingIntent launchIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+    notificationBuilder
+      .setContentIntent(launchIntent)
+      .setContentTitle("Magnitude: " + magnitude)
+      .setContentText(details)
+      .setSubText(date)
+      .setWhen(time);
+
+    if (magnitude > MEDIUM_MAGNITUDE) {
+      notificationBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+    }
+
+    double vibrateLength = 100 * Math.exp(0.53 * magnitude);
+
+    notificationBuilder.setVibrate(new long[] { 100, 100, (long)vibrateLength });
+
+    int color;
+
+    if (magnitude < LIGHT_MAGNITUDE) {
+      color = LIGHT_MAGNITUDE_COLOR;
+    } else if (magnitude < MEDIUM_MAGNITUDE) {
+      color = MEDIUM_MAGNITUDE_COLOR;
+    } else {
+      color = HEAVY_MAGNITUDE_COLOR;
+    }
+
+    notificationBuilder.setLights(color, (int)vibrateLength, (int)vibrateLength);
+
+    NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+    notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+  }
+}
--- app/src/main/java/com/mobiled2/earthquake/UpdateService.java
+++ app/src/main/java/com/mobiled2/earthquake/UpdateService.java
@@ -2,7 +2,6 @@ package com.mobiled2.earthquake;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -10,13 +9,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.graphics.Color;
-import android.media.RingtoneManager;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
-import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import org.w3c.dom.Document;
@@ -38,12 +34,10 @@ import javax.xml.parsers.ParserConfigurationException;
 public class UpdateService extends IntentService {
   private static final String TAG = "EARTHQUAKE_SERVICE";
 
-  public static final int NOTIFICATION_ID = 1;
-
   private AlarmManager alarmManager;
   private PendingIntent alarmIntent;
 
-  private NotificationCompat.Builder notificationBuilder;
+  private Notification notification;
 
   public UpdateService() {
     super("com.mobiled2.earthquake.UpdateService");
@@ -64,12 +58,7 @@ public class UpdateService extends IntentService {
     super.onCreate();
     alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
     alarmIntent = PendingIntent.getBroadcast(this, 0, new Intent(AlarmReceiver.ACTION_REFRESH_EARTHQUAKE_ALARM), 0);
-    notificationBuilder = new NotificationCompat.Builder(this);
-
-    notificationBuilder.setAutoCancel(true)
-      .setDefaults(NotificationCompat.DEFAULT_ALL)
-      .setSmallIcon(R.drawable.ic_notification)
-      .setTicker("Earthquake detected");
+    notification = new Notification(this);
   }
 
   @Override
@@ -130,67 +119,30 @@ public class UpdateService extends IntentService {
     }
   }
 
-  private void addNewQuake(QuakeData quake) {
+  private void addNewQuake(QuakeData quakeData) {
     ContentResolver contentResolver = getContentResolver();
-    Cursor cursor = contentResolver.query(ContentProvider.CONTENT_URI, null, ContentProvider.KEY_DATE + "=" + quake.getDate().getTime(), null, null);
+    Cursor cursor = contentResolver.query(ContentProvider.CONTENT_URI, null, ContentProvider.KEY_DATE + "=" + quakeData.getDate().getTime(), null, null);
 
     if (cursor != null) {
       try {
         if (cursor.getCount() == 0) {
           ContentValues values = new ContentValues();
 
-          values.put(ContentProvider.KEY_DATE, quake.getDate().getTime());
-          values.put(ContentProvider.KEY_DETAILS, quake.getDetails());
-          values.put(ContentProvider.KEY_SUMMARY, quake.toString());
-          values.put(ContentProvider.KEY_LATITUDE, quake.getLatitude());
-          values.put(ContentProvider.KEY_LONGITUDE, quake.getLongitude());
-          values.put(ContentProvider.KEY_DEPTH, quake.getDepth());
-          values.put(ContentProvider.KEY_MAGNITUDE, quake.getMagnitude());
+          values.put(ContentProvider.KEY_DATE, quakeData.getDate().getTime());
+          values.put(ContentProvider.KEY_DETAILS, quakeData.getDetails());
+          values.put(ContentProvider.KEY_SUMMARY, quakeData.toString());
+          values.put(ContentProvider.KEY_LATITUDE, quakeData.getLatitude());
+          values.put(ContentProvider.KEY_LONGITUDE, quakeData.getLongitude());
+          values.put(ContentProvider.KEY_DEPTH, quakeData.getDepth());
+          values.put(ContentProvider.KEY_MAGNITUDE, quakeData.getMagnitude());
 
-          broadcastNotification(quake);
+          notification.send(quakeData);
 
           contentResolver.insert(ContentProvider.CONTENT_URI, values);
         }
       } finally {
         cursor.close();
       }
-    }
-  }
-
-  private void broadcastNotification(QuakeData quakeData) {
-    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-
-    if (prefs.getBoolean(PreferencesActivity.PREF_NOTIFICATION, false)) {
-      PendingIntent launchIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
-
-      notificationBuilder.setContentIntent(launchIntent)
-        .setWhen(quakeData.getDate().getTime())
-        .setContentTitle("Magnitude: " + quakeData.getMagnitude())
-        .setContentText(quakeData.getDetails());
-
-      if (quakeData.getMagnitude() > 6) {
-        notificationBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
-      }
-
-      double vibrateLength = 100 * Math.exp(0.53 * quakeData.getMagnitude());
-
-      notificationBuilder.setVibrate(new long[] { 100, 100, (long)vibrateLength });
-
-      int color;
-
-      if (quakeData.getMagnitude() < 5.4) {
-        color = Color.GREEN;
-      } else if (quakeData.getMagnitude() < 6) {
-        color = Color.YELLOW;
-      } else {
-        color = Color.RED;
-      }
-
-      notificationBuilder.setLights(color, (int)vibrateLength, (int)vibrateLength);
-
-      NotificationManager notificationManager = (NotificationManager)getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-
-      notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
     }
   }
 }
